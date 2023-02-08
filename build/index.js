268678b58da48c0c3e242e96102e29079e9a80c4
Guilherme Sampaio <101369340+sampguilherme@users.noreply.github.com>
2023-02-08 16:36:56 -0300

    Delete build directory

--- build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const database_1 = require("./database");
-(0, database_1.createUser)("05", "ga@mil.com", "jweq");
-console.log((0, database_1.getAllUsers)());
-//# sourceMappingURL=index.js.map
